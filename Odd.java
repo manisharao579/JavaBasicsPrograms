058f9bd3e41b3b0f81cc6eb2198933cdb4fd77a3
Manisha Rao <manisha.rao579@gmail.com>
2025-02-16 16:35:39 +0530

    Delete Odd.java

--- Odd.java
+++
@@ -1,9 +0,0 @@
-public class Odd
-{
-    public static void main(String[] args) {
-        int a=5;
-        if (a % 2 != 0) {
-            System.out.println(" number is Odd.");
-        }
-    }
-}
